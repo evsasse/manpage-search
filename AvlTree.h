35a58fea4113ddfb6d03096c2b72c9d4b7e07856
evsasse <evsasse@gmail.com>
2014-07-11 15:01:12 -0300

    ajsdlkasjdk

---
+++ AvlTree.h
@@ -0,0 +1,206 @@
+#ifndef AVLTREE_H
+#define	AVLTREE_H
+
+#include<cmath>
+using std::pow;
+
+#include <string>
+using std::string;
+
+template <typename T>
+class AvlTree {
+    public:
+    struct Node {
+        Node *left, *right;
+        T data;
+
+        int getHeight() const {
+            if (this == 0) return -1;
+            return height;
+        }
+        
+        Node():
+        left(0),
+        right(0),
+        data(),
+        height(0){
+        }
+
+        Node(const T& t) :
+        left(0),
+        right(0),
+        data(t),
+        height(0) {
+        }
+
+        void updateHeight() {
+            height = max(left->getHeight(), right->getHeight()) + 1;
+        }
+
+        Node* rightMost() {//biggest node of the subtree
+            if (right == 0) return this;
+            return right->rightMost();
+        }
+
+        Node* rotateLeft() {
+            Node* root = right;
+            right = right->left;
+            root->left = this;
+            updateHeight();
+            root->updateHeight();
+            return root;
+        }
+
+        Node* rotateRight() {
+            Node* root = left;
+            left = left->right;
+            root->right = this;
+            updateHeight();
+            root->updateHeight();
+            return root;
+        }
+
+        Node* doubleRotateLeft() {
+            right = right->rotateRight();
+            return rotateLeft();
+        }
+
+        Node* doubleRotateRight() {
+            left = left->rotateLeft();
+            return rotateRight();
+        }
+
+        string toString() {
+            if (this == 0) return "@";
+            return "(" + left->toString() + ")" + data + "(" + right->toString() + ")";
+
+        }
+    private:
+        int height;
+
+        int max(int a, int b) {
+            if (a > b) return a;
+            return b;
+        }
+    };
+public:
+    AvlTree() :
+    root(0) {
+    }
+
+    void insert(const T& t) {
+        root = insert(t, root);
+    }
+
+    void remove(const T& t) {
+        root = remove(t, root);
+    }
+
+    string toString() {//works for <char>
+        return root->toString();
+    }
+    
+    int getSize() const{
+        return size;
+    }
+    
+    int getHeight() const{
+        return root->getHeight();
+    }
+    
+    int getMaxSizeByHeight() const{
+        int h = root->getHeight();
+        h += 1;
+        return (pow(2,h))-1;
+    }
+    
+    T* getByLevel(){ // return T[]
+        //make an array of the nodes
+        int maxSize = getMaxSizeByHeight();
+        Node* arrayN[maxSize];
+        //add the nodes by level
+        arrayN[0] = root;
+        int aaa = 0;
+        //segfault nesse laço
+        for(int i=0;2*i<maxSize;++i){
+            aaa = (2*(i+1))-1;
+            arrayN[aaa] = arrayN[i]->left;
+            aaa++;
+            arrayN[aaa] = arrayN[i]->right;
+        }
+        
+        T* arrayT = new T[maxSize];
+        //add the data from the nodes to arrayT
+        for(int i=0;i<maxSize;++i){
+            arrayT[i] = arrayN[i]->data;
+        }
+        return arrayT;
+    }
+private:
+    Node* root;
+    int size;
+
+    Node* insert(const T& t, Node* root) {
+        if (root == 0) {
+            ++size;
+            return new Node(t);
+        }
+        if (t < root->data) {//left
+            root->left = insert(t, root->left);
+            if (root->left->getHeight() - root->right->getHeight() > 1)
+                if (t < root->left->data)//"/"
+                    root = root->rotateRight();
+                else//"<"
+                    root = root->doubleRotateRight();
+        } else if (t > root->data) {//right
+            root->right = insert(t, root->right);
+            if (root->right->getHeight() - root->left->getHeight() > 1)
+                if (t > root->right->data) //"\"
+                    root = root->rotateLeft();
+                else //">"
+                    root = root->doubleRotateLeft();
+        }
+        root->updateHeight();
+        return root;
+    }
+
+    Node* remove(const T& t, Node* root) {
+        if (root == 0)//data not on the tree
+            return root;
+
+        if (t < root->data) //left
+            root->left = remove(t, root->left);
+        else if(t > root->data) //right
+            root->right = remove(t, root->right);
+        else{//delete root
+            --size;
+            if(root->left == 0 && root->right == 0){//no child
+                delete root;
+                return 0;
+            }else if(root->left == 0 || root->right == 0){//one child
+                Node* aux = (root->left ? root->left : root->right); 
+                delete root;
+                root = aux;
+            }else{//two children
+                Node* aux = root->left->rightMost();
+                root->data = aux->data;
+                root->left = remove(aux->data,root->left);
+                root->updateHeight();
+            }
+        }
+        if (root->left->getHeight() - root->right->getHeight() > 1)
+            if (t > root->left->data)//"/"
+                root = root->rotateRight();
+            else//"<"
+                root = root->doubleRotateRight();
+        else if (root->right->getHeight() - root->left->getHeight() > 1)
+            if (t < root->right->data) //"\"
+                root = root->rotateLeft();
+            else //">"
+                root = root->doubleRotateLeft();
+        root->updateHeight();
+        return root;
+    }
+};
+
+#endif	/* AVLTREE_H */
--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <cstring>
-
-#include "ManPage.h"
+#include <iostream>
 using namespace std;
+#include "ManPage.h"
+#include "AvlTree.h"
+
+
+struct Indice{
+    char comando[100];
+    int posicao;
+    //Define os operadores para poder usar na AvlTree
+    bool operator<(const Indice& rhs) const
+    {
+        if (strcmp(comando,rhs.comando) < 0) return true;
+        return false;
+    }
+    
+    bool operator>(const Indice& rhs) const
+    {
+        if (strcmp(comando,rhs.comando) > 0) return true;
+        return false;
+    }
+};
+
+
 
 void gerarManPagesDat(int argc, char* argv[]);
 
@@ -11,24 +32,47 @@ static const int conteudoTamMax = 149900;
 
 int main(int argc, char* argv[]){
     
-    gerarManPagesDat(argc,argv);
+    AvlTree<char> a;
+    a.insert('a');
+    a.insert('b');
+    a.insert('c');
+    a.insert('d');
+    a.insert('e');
+    a.insert('f');
+    a.insert('g');
+    a.insert('h');
+    cout << a.toString();
+    int maxSize = a.getMaxSizeByHeight();
+    char* b;
+    b = a.getByLevel();
+    for(int i=0; i<maxSize;++i){
+        cout << '\n';
+        cout << b[i];
+    }
     
+    //gerarManPagesDat(argc,argv);
 }
 
 void gerarManPagesDat(int argc, char* argv[]){
+    //Fazendo a árvore em memória para não demorar tanto a geração...
+    AvlTree<Indice> indices;
+    
+    //Abre para sobrescrever ou cria o arquivo manpages.dat
     FILE *manPagesDat;
     manPagesDat = fopen("..\\manpages.dat","w");
     
     for(int i=1;i<argc;++i){
+        //Abre para ler o arquivo de cada manpage
         FILE *manPageF;
         manPageF = fopen(argv[i],"r");
-        
         if (manPageF == NULL) perror ("Erro abrindo arquivo");
         
+        //Gera o nome do comando a partir da entrada de argv[i]], tirando a extensão .txt, e coloca em "comando"
         char comando[comandoTamMax];
         *strrchr(argv[i],'.') = '\0';
         strcpy(comando,argv[i]);
         
+        //Lê o conteúdo do arquivo correspondente e coloca em em "conteudo"
         char conteudo[conteudoTamMax];
         for(int a=0; a<conteudoTamMax; ++a){
             conteudo[a] = ' ';
@@ -41,11 +85,12 @@ void gerarManPagesDat(int argc, char* argv[]){
             if(conteudo[aa] == '\n') ++linhas;
         }
         conteudo[aa-1] = '\0';
-        
+        //Fecha o arquivo que estava lendo
         fclose(manPageF);
         
         bool charFinal = false;
-        for(int a=0;a<comandoTamMax;++a){//Escreve o comando
+        //Escreve o comando em manpages.dat
+        for(int a=0;a<comandoTamMax;++a){
             if(charFinal)
                 fputc(' ',manPagesDat);
             else{
@@ -56,11 +101,26 @@ void gerarManPagesDat(int argc, char* argv[]){
                     fputc(comando[a],manPagesDat);
             }
         }
-        charFinal = false;
-        for(int a=0;a<conteudoTamMax-linhas;++a){//Escreve o conteúdo
+        //Escreve o conteúdo em manpages.dat
+        //Diminui a quantidade de linhas pois os \n contam como 2 caracteres, então 2 bytes, "pula a linha, e retorno do carro"
+        for(int a=0;a<conteudoTamMax-linhas;++a){
             fputc(conteudo[a],manPagesDat);
         }
+        //Finalizou a escrita em manPagesDat
+        
+        //Adiciona o arquivo e seu indice na árvore de indice principal
+        //"i-1" corresponde a posição que foi salvo o registro do comando em manpages.dat
+        Indice manPage;
+        *manPage.comando = *comando;
+        manPage.posicao = i-1;
+        indices.insert(manPage);
     }
+    
+    //A arvore "indices" deve conter todos os indices ordenados em forma de árvore para serem salvos em disco
+    //Ela é retornada em ordem de nível para ser escrita nessa ordem
+    //tree.root->height
+    Indice indicesOrdenados[indices.getMaxSizeByHeight()];
+    //indices.getByLevel(indicesOrdenados);
     
     fclose(manPagesDat);
 }
